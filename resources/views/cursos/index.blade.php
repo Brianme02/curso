8e01daba8ba14aa23b29728a1f035c638bed34e2
Axel V <axelvalentin34@gmail.com>
2025-08-07 09:47:04 -0600

    Modificacion_Axel

--- resources/views/cursos/index.blade.php
+++ resources/views/cursos/index.blade.php
@@ -3,5 +3,5 @@
 @section('title', 'Cursos')
 
 @section('content')
-    <h1>Bienvenido a la pagina principal de cursos</h1>
+    <h1>Bienvenido a la pagina principal de cursos de brayan</h1>
 @endsection
